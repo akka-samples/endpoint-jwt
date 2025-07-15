b0aa04c2f7a57f97d521932c74523d54088e9dd6
lightbend-tools-secure <148421913+lightbend-tools-secure@users.noreply.github.com>
2025-07-15 10:11:28 -0400

    chore: bump SDK version to 3.4.4 (#21)

--- src/main/java/hellojwt/api/HelloJwtEndpoint.java
+++ src/main/java/hellojwt/api/HelloJwtEndpoint.java
@@ -2,14 +2,13 @@ package hellojwt.api;
 
 import akka.javasdk.annotations.Acl;
 import akka.javasdk.annotations.JWT;
-import akka.javasdk.annotations.http.HttpEndpoint;
 
 import akka.javasdk.annotations.http.Get;
+import akka.javasdk.annotations.http.HttpEndpoint;
 import akka.javasdk.http.AbstractHttpEndpoint;
 
-
 // Opened up for access from the public internet to make the sample service easy to try out.
-// For actual services meant for production this must be carefully considered, and often set more limited
+// For actual services meant for production this must be carefully considered and often set more limited
 @Acl(allow = @Acl.Matcher(principal = Acl.Principal.INTERNET))
 @HttpEndpoint("/hello")
 @JWT(validate = JWT.JwtMethodMode.BEARER_TOKEN, bearerTokenIssuers = "my-issuer") // <1>
@@ -20,8 +19,11 @@ public class HelloJwtEndpoint extends AbstractHttpEndpoint {
     return "Hello, World!";
   }
 
-
-  @JWT(validate = JWT.JwtMethodMode.BEARER_TOKEN, bearerTokenIssuers = {"my-issuer", "my-issuer2"}, staticClaims = @JWT.StaticClaim(claim = "sub", values = "my-subject"))
+  @JWT(
+    validate = JWT.JwtMethodMode.BEARER_TOKEN,
+    bearerTokenIssuers = { "my-issuer", "my-issuer2" },
+    staticClaims = @JWT.StaticClaim(claim = "sub", values = "my-subject")
+  )
   @Get("/claims")
   public String helloClaims() {
     var claims = requestContext().getJwtClaims(); // <1>
@@ -29,6 +31,4 @@ public class HelloJwtEndpoint extends AbstractHttpEndpoint {
     var sub = claims.subject().get(); // <2>
     return "issuer: " + issuer + ", subject: " + sub;
   }
-
 }
-
